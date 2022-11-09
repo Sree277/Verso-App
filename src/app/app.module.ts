4f5d3bffaf7ae82ead8be6f62d615189616bcd72
Sree <sreepriya5155@gmail.com>
2022-11-09 18:37:00 +0400

    create versoapp and add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchNavComponent } from './search-nav/search-nav.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchNavComponent
+    SearchNavComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
