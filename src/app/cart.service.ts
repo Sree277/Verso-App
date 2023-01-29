23f673ac4b62a1e38037bd395877bcf3a209bdc4
Sree277 <sreepriya5155@gmail.com>
2023-01-29 22:53:29 +0530

    CartService changes

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -10,18 +10,24 @@ export class CartService {
   cartList: Array<any> = [];
 
   addToCart(id: number, quantity: number) {
-    this.cartList.push({ id: id, quantity: quantity });
+    let cartItem = this.cartList.find((item) => item.id == id);
+    if (cartItem) {
+      cartItem.quantity = cartItem.quantity + quantity;
+    }
+    else {
+      this.cartList.push({ id: id, quantity: quantity });
+    }
   }
 
   getCartList() {
-    let carrListData: Array<any> = [];
+    let cartListData: Array<any> = [];
 
     this.cartList.forEach(item => {
       let productData = this.bookService.getBookById(item.id);
-      carrListData.push({ id: item.id, quantity: item.quantity, productData: productData })
+      cartListData.push({ id: item.id, quantity: item.quantity, productData: productData })
     });
 
-    return carrListData;
+    return cartListData;
   }
 
 }
